3b07568a3792495cf82b628c7a35310488da95d0
Christopher Neugebauer <chrisjrn@gmail.com>
2016-01-18 14:44:05 +1100

    Adds Discount as a foreign key to DiscountForProduct

--- symposion/registration/models.py
+++ symposion/registration/models.py
@@ -144,6 +144,7 @@ class DiscountForProduct(models.Model):
             raise ValidationError(
                 _("Discount may only have a percentage or only a price."))
 
+    discount = models.ForeignKey(DiscountBase, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     percentage = models.DecimalField(max_digits=4, decimal_places=1, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=True)
