babe47c49520888e662de76ee8e34bdce6fcc2cc
Christopher Neugebauer <chrisjrn@gmail.com>
2017-01-13 09:05:47 +1100

    Unbreaks URLs

--- symposion/schedule/urls.py
+++ symposion/schedule/urls.py
@@ -22,6 +22,9 @@ urlpatterns = [
     url(r"^list/$", schedule_list, name="schedule_list"),
     url(r"^presentations.csv$", schedule_list_csv, name="schedule_list_csv"),
     url(r"^presentation/(\d+)/$", schedule_presentation_detail, name="schedule_presentation_detail"),
+    url(r"^sessions/staff.txt$", session_staff_email, name="schedule_session_staff_email"),
+    url(r"^sessions/$", session_list, name="schedule_session_list"),
+    url(r"^session/(\d+)/$", session_detail, name="schedule_session_detail"),
     url(r"^([\w\-]+)/$", schedule_detail, name="schedule_detail"),
     url(r"^([\w\-]+)/edit/$", schedule_edit, name="schedule_edit"),
     url(r"^([\w\-]+)/list/$", schedule_list, name="schedule_list"),
@@ -29,7 +32,4 @@ urlpatterns = [
     url(r"^([\w\-]+)/edit/slot/(\d+)/", schedule_slot_edit, name="schedule_slot_edit"),
     url(r"^conference.json", schedule_json, name="schedule_json"),
     url(r"^conference.ics", EventFeed(), name="ical_feed"),
-    url(r"^sessions/staff.txt$", session_staff_email, name="schedule_session_staff_email"),
-    url(r"^sessions/$", session_list, name="schedule_session_list"),
-    url(r"^session/(\d+)/$", session_detail, name="schedule_session_detail"),
 ]
